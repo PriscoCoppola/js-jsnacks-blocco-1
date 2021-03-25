5ae8f28c9100d708715562524b85dfe7290d45f5
PriscoCoppola <priscocoppola7@gmail.com>
2021-03-25 15:04:00 +0100

    initialize

---
+++ js/main.js
@@ -0,0 +1,5 @@
+/*
+    Snack 5 - Bonus
+    Crea due array di numeri che hanno un numero di elementi diversi.
+    Aggiungi elementi all’array che ha meno elementi fino a quando ne avrà tanti quanti l’altro.
+*/
