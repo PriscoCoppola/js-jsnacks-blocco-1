5156c748a150798e37434f5620a677f8c267b51a
PriscoCoppola <priscocoppola7@gmail.com>
2021-03-25 15:41:52 +0100

    add snack bonus

--- js/main.js
+++ js/main.js
@@ -3,3 +3,19 @@
     Crea due array di numeri che hanno un numero di elementi diversi.
     Aggiungi elementi all’array che ha meno elementi fino a quando ne avrà tanti quanti l’altro.
 */
+
+var arrayUno = [1, 6, 36, 47];
+var arrayDue = [5, 65];
+
+if (arrayUno.length > arrayDue.length) {
+    for (var i = arrayDue.length; i < arrayUno.length; i++) {
+        arrayDue.push( Math.floor( Math.random() * 100 ) );
+    }
+} else {
+    for (var i = arrayUno.length; i < arrayDue.length; i++) {
+        arrayUno.push( Math.floor( Math.random() * 100 ) );
+    }
+}
+
+console.log(arrayUno);
+console.log(arrayDue);
